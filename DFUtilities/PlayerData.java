74bab3cc2c00ffff4867a68e065142d536c29748
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-06-10 19:43:08 -0500

    🐞Small change to the declaration of an array.
    It is now a list 👍

--- DFUtilities/PlayerData.java
+++ DFUtilities/PlayerData.java
@@ -2,15 +2,14 @@ package me.wonk2.Utilities;
 
 import org.bukkit.Material;
 
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 public final class PlayerData { // Permanently borrowed PlayerData class from CheaterTim
   public boolean canPvP = true;
   public boolean deathDrops = false;
   public boolean keepInv = false;
   public boolean instantRespawn = false;
-  public Material[] allowedBlocks = new Material[0];
+  public List<Material> allowedBlocks = new LinkedList<>();
   
   public static final HashMap<UUID, PlayerData> data = new HashMap<>();
   public static PlayerData getPlayerData(UUID uuid) {
