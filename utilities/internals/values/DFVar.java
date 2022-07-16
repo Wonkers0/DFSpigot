3c0e22dedb14da0b10230992e74eb477e961f0a2
Lemon Nerd <85891155+CheaterTim@users.noreply.github.com>
2022-07-16 16:33:07 +0200

    SAVE variables

--- utilities/internals/values/DFVar.java
+++ utilities/internals/values/DFVar.java
@@ -2,8 +2,17 @@ package me.wonk2.utilities.internals.values;
 
 import me.wonk2.utilities.internals.enums.DFType;
 import me.wonk2.utilities.internals.enums.Scope;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import static me.wonk2.utilities.DFUtilities.varConfig;
+import static me.wonk2.utilities.internals.enums.DFType.*;
 
 public class DFVar {
     public static HashMap<String, DFValue> globalVars = new HashMap<>();
@@ -30,11 +39,61 @@ public class DFVar {
         return varStorage.containsKey(var.name) ? varStorage.get(var.name) : nullVar();
     }
 
+    private static final String DOT = "§";
+
     public static void setVar(DFVar var, DFValue value, HashMap<String, DFValue> localStorage){
         // Use this to assign a value to a variable.
         if(var.scope == Scope.GLOBAL) globalVars.put(var.name, value);
         else if(var.scope == Scope.LOCAL) localStorage.put(var.name, value);
-        else if(var.scope == Scope.SAVE) /*TODO: Save variable to .yml file*/;
+        else if(var.scope == Scope.SAVE) {
+            FileConfiguration config = varConfig.getConfig();
+
+            String poorlySanitizedName = var.name.replace(".", DOT);
+
+            String varPath = "vars." + poorlySanitizedName;
+
+            //config.set("vars." + var.name.replace(".", DOT) + ".val", value.getVal());
+
+            switch(value.type) {
+                case TXT:
+                case NUM:
+                    config.set(varPath + ".val", value.getVal());
+                    break;
+                case SND:
+                    DFSound sound = (DFSound) value.getVal();
+                    config.set(varPath + ".val.name", sound.sound.toString());
+                    config.set(varPath + ".val.volume", sound.volume);
+                    config.set(varPath + ".val.pitch", sound.pitch);
+                    break;
+                case LOC:
+                    Location location = (Location) value.getVal();
+                    config.set(varPath + ".val.x", location.getX());
+                    config.set(varPath + ".val.y", location.getY());
+                    config.set(varPath + ".val.z", location.getZ());
+                    config.set(varPath + ".val.pitch", location.getPitch());
+                    config.set(varPath + ".val.yaw", location.getYaw());
+                    config.set(varPath + ".val.world", location.getWorld().getName());
+                    break;
+                case ITEM:
+                    ItemStack item = (ItemStack) value.getVal();
+                    Map<String, Object> serialized = item.serialize();
+                    for(String key : serialized.keySet()) {
+                        config.set(varPath + ".val." + key, serialized.get(key));
+                    }
+                    break;
+                case POT:
+                    PotionEffect potion = (PotionEffect) value.getVal();
+                    config.set(varPath + ".val.type", potion.getType().getName());
+                    config.set(varPath + ".val.duration", potion.getDuration());
+                    config.set(varPath + ".val.amplifier", potion.getAmplifier());
+                    config.set(varPath + ".val.ambient", potion.isAmbient());
+                    config.set(varPath + ".val.particles", potion.hasParticles());
+                    break;
+            }
+
+            config.set("vars." + var.name.replace(".", DOT) + ".type", value.type.toString());
+            varConfig.saveConfig();
+        }
     }
 
     private static DFValue nullVar(){
