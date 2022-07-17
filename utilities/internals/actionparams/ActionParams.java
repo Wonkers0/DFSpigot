0d149fd855365063292812c10ea11fb73b0c6d6c
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-17 07:20:40 -0700

    Create ActionParams.java

---
+++ utilities/internals/actionparams/ActionParams.java
@@ -0,0 +1,9 @@
+package me.wonk2.utilities.internals.actionparams;
+
+import me.wonk2.utilities.internals.values.Parameter;
+
+import java.util.HashMap;
+
+public class ActionParams {
+    public HashMap<String, Parameter[]> argMap;
+}
