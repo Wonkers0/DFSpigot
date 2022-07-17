9107639ff2e896ed47e1d01bdf71532292a8f93b
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-17 14:32:37 -0700

    ✅ Omitting optional parameters is now supported.
    The parameter pattern for actions is now also grabbed from the repository to save space.

--- utilities/internals/ParamManager.java
+++ utilities/internals/ParamManager.java
@@ -1,344 +1,30 @@
 package me.wonk2.utilities.internals;
 
-import me.wonk2.utilities.internals.enums.*;
-import me.wonk2.utilities.internals.values.*;
-import org.bukkit.inventory.ItemStack;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.wonk2.utilities.internals.otherutils.ActionParams;
+import me.wonk2.utilities.internals.enums.DFType;
+import me.wonk2.utilities.internals.otherutils.DownloadUtils;
+import me.wonk2.utilities.internals.values.DFValue;
+import me.wonk2.utilities.internals.values.DFVar;
+import me.wonk2.utilities.internals.values.Parameter;
 
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ParamManager {
-    public static HashMap<String, Parameter[]> argInfo = new HashMap<>(){{
-        put("SendMessage", new Parameter[]{
-            new Parameter("msg", DFType.TXT, null, true)
-        });
-
-        put("PlaySound", new Parameter[]{
-            new Parameter("sounds", DFType.SND, null, true),
-            new Parameter("playbackLoc", DFType.LOC, null, false)
-        });
-
-        put("SendTitle", new Parameter[]{
-            new Parameter("title", DFType.TXT, null, false),
-            new Parameter("subtitle", DFType.TXT, "", false),
-            new Parameter("duration", DFType.NUM, 60, false),
-            new Parameter("fadeIn", DFType.NUM, 20, false),
-            new Parameter("fadeOut", DFType.NUM, 20, false)
-        });
-
-        put("SetBossBar", new Parameter[]{
-            new Parameter("title", DFType.TXT, "", false),
-            new Parameter("health", DFType.NUM, 100d, false),
-            new Parameter("maxHealth", DFType.NUM, 100d, false),
-            new Parameter("index", DFType.NUM, 1, false)
-        });
-
-        put("RemoveBossBar", new Parameter[]{
-           new Parameter("index", DFType.NUM, null, false)
-        });
-
-        put("ActionBar", new Parameter[]{
-            new Parameter("msg", DFType.TXT, null, true)
-        });
-
-        put("SendMessageSeq", new Parameter[]{
-            new Parameter("msgs", DFType.TXT, null, true),
-            new Parameter("delay", DFType.NUM, 60, false)
-        });
-
-        put("PlaySoundSeq", new Parameter[]{
-           new Parameter("sounds", DFType.SND, null, true),
-           new Parameter("delay", DFType.NUM,  60, false),
-           new Parameter("playbackLoc", DFType.LOC, null, false)
-        });
-
-        put("SendHover", new Parameter[]{
-           new Parameter("msg", DFType.TXT, null, false),
-           new Parameter("hover", DFType.TXT, null, false)
-        });
-
-        put("StopSound", new Parameter[]{
-           new Parameter("sounds", DFType.SND, null, true)
-        });
-
-        put("SetTabListInfo", new Parameter[]{
-           new Parameter("tabInfo", DFType.TXT, null, false)
-        });
-
-        put("SetHotbar", new Parameter[]{
-           new Parameter("items", DFType.ITEM, new ItemStack[9], true)
-        });
-
-        put("GiveItems", new Parameter[]{
-           new Parameter("items", DFType.ITEM, null, true)
-        });
-
-        put("SetInventory", new Parameter[]{
-           new Parameter("items", DFType.ITEM, new ItemStack[27], true)
-        });
-
-        put("SetSlotItem", new Parameter[]{
-           new Parameter("item", DFType.ITEM, null, false),
-           new Parameter("slot", DFType.NUM, null, false)
-        });
-
-        put("SetEquipment", new Parameter[]{
-           new Parameter("item", DFType.ITEM, null, false)
-        });
-
-        put("SetArmor", new Parameter[]{
-           new Parameter("head", DFType.ITEM, null, false),
-           new Parameter("chest", DFType.ITEM, null, false),
-           new Parameter("leggings", DFType.ITEM, null, false),
-           new Parameter("feet", DFType.ITEM, null, false)
-        });
-
-        put("ReplaceItems", new Parameter[]{
-           new Parameter("replaceables", DFType.ITEM, new ItemStack[1], true),
-           new Parameter("replacement", DFType.ITEM, null, false),
-           new Parameter("amount", DFType.NUM, 1, false)
-        });
-
-        put("RemoveItems", new Parameter[]{
-           new Parameter("removals", DFType.ITEM, null, true)
-        });
-
-        put("ClearItems", new Parameter[]{
-           new Parameter("items", DFType.ITEM, null, true)
-        });
-
-        put("SetCursorItem", new Parameter[]{
-           new Parameter("item", DFType.ITEM, null, false)
-        });
-
-        put("ClearInv", new Parameter[0]);
-
-        put("SetItemCooldown", new Parameter[]{
-           new Parameter("item", DFType.ITEM, null, false),
-           new Parameter("ticks", DFType.NUM, null, false)
-        });
-
-        put("SaveInv", new Parameter[0]);
-
-        put("LoadInv", new Parameter[0]);
-
-        put("ShowInv", new Parameter[]{
-           new Parameter("items", DFType.ITEM, new ItemStack[0], true)
-        });
-
-        put("ExpandInv", new Parameter[]{
-           new Parameter("items", DFType.ITEM, new ItemStack[0], true)
-        });
-
-        put("SetMenuItem", new Parameter[]{
-           new Parameter("slot", DFType.NUM, null, false),
-           new Parameter("item", DFType.ITEM, null, false)
-        });
-
-        put("SetInvName", new Parameter[]{
-           new Parameter("invName", DFType.TXT, null, false)
-        });
-
-        put("CloseInv", new Parameter[0]);
-
-        put("RemoveInvRow", new Parameter[]{
-           new Parameter("rows", DFType.NUM, 1, false)
-        });
-
-        put("AddInvRow", new Parameter[]{
-           new Parameter("items", DFType.ITEM, new ItemStack[9], true)
-        });
-
-        put("OpenBlockInv", new Parameter[]{
-           new Parameter("loc", DFType.LOC, null, false)
-        });
-
-        put("Damage", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false),
-           new Parameter("source", DFType.TXT, null, false)
-        });
-
-        put("Heal", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetHealth", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetMaxHealth", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetAbsorption", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetFoodLevel", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetSaturation", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("GiveExp", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetExp", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("GivePotion", new Parameter[]{
-           new Parameter("effects", DFType.POT, null, true)
-        });
-
-        put("RemovePotion", new Parameter[]{
-           new Parameter("effects", DFType.POT, null, true)
-        });
-
-        put("ClearPotions", new Parameter[0]);
-
-        put("SetSlot", new Parameter[]{
-           new Parameter("slot", DFType.NUM, null, false)
-        });
-
-        put("SetAtkSpeed", new Parameter[]{
-           new Parameter("amount", DFType.NUM, null, false)
-        });
-
-        put("SetFireTicks", new Parameter[]{
-           new Parameter("ticks", DFType.NUM, null, false)
-        });
-
-        put("SetFreezeTicks", new Parameter[]{
-           new Parameter("ticks", DFType.NUM, null, false)
-        });
-
-        put("SetAirTicks", new Parameter[]{
-           new Parameter("ticks", DFType.NUM, null, false)
-        });
-
-        put("SetInvulTicks", new Parameter[]{
-           new Parameter("ticks", DFType.NUM, null, false)
-        });
-
-        put("SetFallDistance", new Parameter[]{
-           new Parameter("distance", DFType.NUM, null, false)
-        });
-
-        put("SetSpeed", new Parameter[]{
-           new Parameter("speed", DFType.NUM, null, false)
-        });
-
-        put("SetAllowFlight", new Parameter[0]);
-
-        put("SetDropsEnabled", new Parameter[0]);
-
-        put("SetInventoryKept", new Parameter[0]);
-
-        put("SetCollidable", new Parameter[0]);
-
-        put("InstantRespawn", new Parameter[0]);
-
-        put("EnableBlocks", new Parameter[]{
-           new Parameter("blocks", DFType.ITEM, null, true)
-        });
-
-        put("DisableBlocks", new Parameter[]{
-           new Parameter("blocks", DFType.ITEM, null, true)
-        });
-
-        // ↑ Player Action ////////////////////////////////////////////////////////////////////////
-        // ↓ Set Variable  ////////////////////////////////////////////////////////////////////////
-
-        put("=", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("value", DFType.ANY, null, false)
-        });
-
-        put("RandomValue", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("values", DFType.ANY, null, true)
-        });
-
-        put("PurgeVars", new Parameter[]{
-           new Parameter("varNames", DFType.TXT, null, true)
-        });
-        
-        put("+", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, null, true)
-        });
-
-        put("-", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, null, true)
-        });
-
-        put("x", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, null, true)
-        });
-
-        put("/", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, null, true)
-        });
-
-        put("%", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("dividend", DFType.NUM, null, false),
-           new Parameter("divisor", DFType.NUM, null, false)
-        });
-
-        put("+=", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, new Integer[]{1}, true)
-        });
-
-        put("-=", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("nums", DFType.NUM, new Integer[]{1}, true)
-        });
-
-        put("Exponent", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("num", DFType.NUM, null, false),
-           new Parameter("exponent", DFType.NUM, 2, false)
-        });
-
-        put("Root", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("num", DFType.NUM, null, false),
-           new Parameter("rootIndex", DFType.NUM, 2, false)
-        });
-
-        put("Logarithm", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("num", DFType.NUM, null, false),
-           new Parameter("base", DFType.NUM, null, false)
-        });
-
-        put("ParseNumber", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("txt", DFType.TXT, null, false)
-        });
-
-        put("AbsoluteValue", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("num", DFType.NUM, null, false)
-        });
-
-        put("ClampNumber", new Parameter[]{
-           new Parameter("var", DFType.VAR, null, false),
-           new Parameter("num", DFType.VAR, null, false)
-        });
-    }};
+    public static HashMap<String, Parameter[][]> argInfo;
+
+    static {
+        try {
+            argInfo = new ObjectMapper().readValue(DownloadUtils.getString("https://raw.githubusercontent.com/Wonkers0/DFSpigot/main/utilities/internals/actionparams/actionparams.json"), ActionParams.class).argMap;
+        } catch (IOException ignored) {}
+    }
 
     public static Object[] formatParameters(HashMap<Integer, DFValue> input, HashMap<String, String> tags, String methodName, HashMap<String, DFValue> localStorage) {
         HashMap<String, DFValue> args = new HashMap<>();
-        Parameter[] params = argInfo.get(methodName);
+        Parameter[] params = getParamTemplate(input, argInfo.get(methodName), localStorage);
 
         ArrayList<Integer> keySet = new ArrayList<>(input.keySet());
         int paramIndex = 0;
@@ -385,5 +71,26 @@ public class ParamManager {
         }
 
         return new Object[]{args, tags};
+    }
+
+    private static Parameter[] getParamTemplate(HashMap<Integer, DFValue> input, Parameter[][] templates, HashMap<String, DFValue> localStorage){
+        if(templates.length == 1) return templates[0];
+
+        for(Parameter[] template : templates){
+            for(int i = 0; i < template.length; i++){
+                if(!input.containsKey(i)) break;
+                DFValue currentArg = input.get(i);
+
+                if(currentArg.type == DFType.VAR && template[i].type != DFType.VAR)
+                    // Do we need the var itself here, or its value? ^
+                    currentArg = DFVar.getVar((DFVar) currentArg.getVal(), localStorage); // Get var value
+
+                if(currentArg.type != template[i].type) break;
+
+                if(i == template.length - 1) return template;
+            }
+        }
+
+        return templates[0];
     }
 }
