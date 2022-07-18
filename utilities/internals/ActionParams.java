579a4e81e5874cc5ff890c78b920b76a11dff4dc
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-18 03:44:33 -0700

    Add files via upload

---
+++ utilities/internals/ActionParams.java
@@ -0,0 +1,9 @@
+package me.wonk2.utilities.internals;
+
+import me.wonk2.utilities.values.Parameter;
+
+import java.util.HashMap;
+
+public class ActionParams {
+    public HashMap<String, Parameter[][]> argMap;
+}
