1bed8338936d82161eb204c577d1f33740a13b8d
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-18 03:43:57 -0700

    Deleting for bulk file upload

--- utilities/internals/otherutils/ActionParams.java
+++
@@ -1,9 +0,0 @@
-package me.wonk2.utilities.internals.otherutilities;
-
-import me.wonk2.utilities.internals.values.Parameter;
-
-import java.util.HashMap;
-
-public class ActionParams {
-    public HashMap<String, Parameter[][]> argMap;
-}
