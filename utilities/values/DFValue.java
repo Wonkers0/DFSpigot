b9b21c9bbda5d9d94b1dc8616bfc6ccfd36a0058
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-21 03:05:01 -0700

    Changed return value of castTxt

--- utilities/values/DFValue.java
+++ utilities/values/DFValue.java
@@ -1,11 +1,8 @@
 package me.wonk2.utilities.values;
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import me.wonk2.utilities.DFUtilities;
 import me.wonk2.utilities.enums.DFType;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.TagParser;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
@@ -13,19 +10,11 @@ import java.util.Arrays;
 
 public class DFValue {
 
-    public static ItemStack parseItemNBT(String rawNBT){
-        if(rawNBT == "null") return null;
-        CompoundTag nbt = null;
-        try{nbt = TagParser.parseTag(rawNBT);}
-        catch(CommandSyntaxException e){}
-
-        net.minecraft.world.item.ItemStack nmsItem = net.minecraft.world.item.ItemStack.of(nbt);
-        return CraftItemStack.asBukkitCopy(nmsItem);
-    }
-
     public static String[] castTxt(DFValue[] arr){
-        Object[] values = getValues(arr);
-        return Arrays.copyOf(values, values.length, String[].class);
+        String[] result = new String[arr.length];
+        for(int i = 0; i < arr.length; i++) result[i] = DFUtilities.parseTxt(arr[i]);
+        
+        return result;
     }
 
     public static double[] castNum(DFValue[] arr){
