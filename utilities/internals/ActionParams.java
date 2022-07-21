7b14e97ba663389dfff52bb6c64334b22064227d
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-21 15:25:08 -0700

    Wrapper class not needed anymore
    Was previously used for deserializing the json for action params, but it is now deserialized directly into a hashmap.

--- utilities/internals/ActionParams.java
+++
@@ -1,9 +0,0 @@
-package me.wonk2.utilities.internals;
-
-import me.wonk2.utilities.values.Parameter;
-
-import java.util.HashMap;
-
-public class ActionParams {
-    public HashMap<String, Parameter[][]> argMap;
-}
