efc60681a5b13968e51e6bce6dd6056edb42b461
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-30 04:14:21 -0500

    Deserialization fix

--- utilities/values/Parameter.java
+++ utilities/values/Parameter.java
@@ -3,10 +3,10 @@ package me.wonk2.utilities.values;
 import me.wonk2.utilities.enums.DFType;
 
 public class Parameter {
-    public final String paramName;
-    public final DFType type;
-    public final Object defaultValue;
-    public final boolean repeating;
+    public String name;
+    public DFType type;
+    public Object defaultValue;
+    public boolean repeating;
 
     public Parameter(String paramName, DFType type, Object defaultValue, boolean repeating){
         this.type = type;
@@ -14,4 +14,6 @@ public class Parameter {
         this.repeating = repeating;
         this.paramName = paramName;
     }
+    
+    public Parameter() {} // Empty constructor needed for jackson deserialization library
 }
