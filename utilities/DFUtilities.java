55000c9f191a2c19f8a66736cca0da60659fc09d
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-07-30 02:13:07 -0500

    Added formatTime method

--- utilities/DFUtilities.java
+++ utilities/DFUtilities.java
@@ -40,133 +40,137 @@ import java.util.regex.Pattern;
 
 // Class to store general-use methods and implement event handlers to assist other action implementations
 public class DFUtilities implements Listener {
-    public static FileManager playerConfig;
-    public static FileManager varConfig;
-
-    public static boolean inCustomInv(Player p){
-        Inventory inv = p.getOpenInventory().getTopInventory();
-        return inv.getType() != InventoryType.PLAYER
-                && inv.getType() != InventoryType.CRAFTING
-                && inv.getLocation() == null;
-    }
-    
-    public static boolean locationEquals(Location loc, Location loc2, boolean ignoreRotation) {
-        return loc.getX() == loc2.getX() && loc.getY() == loc2.getY() && loc.getZ() == loc2.getZ() && (ignoreRotation || loc.getYaw() == loc2.getYaw() && loc.getPitch() == loc2.getPitch());
-    }
-
-    public static double clampNum(double num, double min, double max){
-        if(num > max) num = max;
-        else if (num < min) num = min;
-
-        return num;
-    }
-
-    public static double wrapNum(double num, double min, double max){
-        if(num > max) num = min;
-        else if (num < min) num = max;
-
-        return num;
-    }
-
-    public static String parseTxt(DFValue val){
-        switch(val.type){
-            case LIST: {
-                DFValue[] elements = (DFValue[]) val.getVal();
-                String[] result = new String[elements.length];
-                for(int i = 0; i < elements.length; i++) result[i] = parseTxt(elements[i]);
-                return "[" +  String.join(", ", result) + "]";
-            }
-            case ITEM: {
-                ItemMeta meta = ((ItemStack) val.getVal()).getItemMeta();
-
-                if(meta != null && meta.hasDisplayName())
-                    return meta.getDisplayName();
-                else return "";
-            }
-            case POT: {
-                PotionEffect effect = (PotionEffect) val.getVal();
-                String name = StringUtils.capitalise(effect.getType().getName().replace('_', ' '));
-                long timestamp = effect.getDuration() / 20 * 1000L;
-                SimpleDateFormat dateFormat = new SimpleDateFormat("mm:s");
-
-                return name + " " + effect.getAmplifier() + " - " + dateFormat.format(new Date(timestamp));
-            }
-            case LOC: {
-                Location loc = (Location) val.getVal();
-                if(loc.getYaw() == 0f && loc.getPitch() == 0f)
-                    return "[" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + "]";
-                else return "[" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ", " + loc.getPitch() + ", " + loc.getYaw() + "]";
-            }
-            case SND: {
-                DFSound sound = (DFSound) val.getVal();
-                return sound.getName() + "[" + sound.pitch + "]" + "[" + sound.volume + "]";
-            }
-            default: {
-                return String.valueOf(val.getVal());
-            }
-        }
-    }
-    
-    public static String[] parseTxt(DFValue[] vals){
-        String[] result = new String[vals.length];
-        for(int i = 0; i < vals.length; i++) result[i] = parseTxt(vals[i]);
-        
-        return result;
-    }
-    
-    public static String escapeRegex(String input){
-        return input.replaceAll("[-.+*?\\[^\\]$(){}=!<>|:\\\\]", "\\\\$0");
-    }
-    
-    public static HashMap<String, DFValue> getArgs(Object obj){
-        return (HashMap<String, DFValue>) obj;
-    }
-
-    public static HashMap<String, String> getTags(Object obj){
-        return (HashMap<String, String>) obj;
-    }
-
-    @EventHandler
-    public static void PlayerLeave(PlayerQuitEvent event){
-        if(Bukkit.getOnlinePlayers().size() == 1) DFVar.globalVars = new HashMap<>(); // Purge all global vars when all players leave
-    }
-
-    @EventHandler
-    public static void ClickMenuSlot(InventoryClickEvent event){
-        if(inCustomInv((Player) event.getView().getPlayer())) event.setCancelled(true); // ClickSlot event triggered from inside custom GUI
-    }
-
-    @EventHandler
-    public static void PlayerDmgPlayer(EntityDamageByEntityEvent event){
-        if(event.getDamager() instanceof Player && event.getEntity() instanceof Player){
-            if(!PlayerData.getPlayerData(((Player) event.getEntity()).getUniqueId()).canPvP)
-                event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public static void PlaceBlock(BlockPlaceEvent event){
-        PlayerData playerData = PlayerData.getPlayerData(event.getPlayer().getUniqueId());
-        if(!playerData.allowedBlocks.contains(event.getBlockPlaced().getType())) event.setCancelled(true);
-    }
-
-    @EventHandler
-    public static void BreakBlock(BlockBreakEvent event){
-        PlayerData playerData = PlayerData.getPlayerData(event.getPlayer().getUniqueId());
-        if(!playerData.allowedBlocks.contains(event.getBlock().getType())) event.setCancelled(true);
-    }
-
-    @EventHandler
-    public static void Death(PlayerDeathEvent event){
-        PlayerData playerData = PlayerData.getPlayerData(((Player) event.getEntity()).getUniqueId());
-        if(!playerData.deathDrops) event.getDrops().clear();
-        if(playerData.keepInv) event.setKeepInventory(true);
-        if(playerData.instantRespawn) ((Player) event.getEntity()).spigot().respawn();
-    }
-
-    public static void getManagers(JavaPlugin plugin){
-        playerConfig = new FileManager(plugin, "playerData.yml");
-        varConfig = new FileManager(plugin, "varData.yml");
-    }
-
+	public static FileManager playerConfig;
+	public static FileManager varConfig;
+	
+	public static boolean inCustomInv(Player p){
+		Inventory inv = p.getOpenInventory().getTopInventory();
+		return inv.getType() != InventoryType.PLAYER
+			&& inv.getType() != InventoryType.CRAFTING
+			&& inv.getLocation() == null;
+	}
+	
+	public static boolean locationEquals(Location loc, Location loc2, boolean ignoreRotation) {
+		return loc.getX() == loc2.getX() && loc.getY() == loc2.getY() && loc.getZ() == loc2.getZ() && (ignoreRotation || loc.getYaw() == loc2.getYaw() && loc.getPitch() == loc2.getPitch());
+	}
+	
+	public static double clampNum(double num, double min, double max){
+		if(num > max) num = max;
+		else if (num < min) num = min;
+		
+		return num;
+	}
+	
+	public static double wrapNum(double num, double min, double max){
+		if(num > max) num = min;
+		else if (num < min) num = max;
+		
+		return num;
+	}
+	
+	public static String parseTxt(DFValue val){
+		switch(val.type){
+			case LIST: {
+				DFValue[] elements = (DFValue[]) val.getVal();
+				String[] result = new String[elements.length];
+				for(int i = 0; i < elements.length; i++) result[i] = parseTxt(elements[i]);
+				return "[" +  String.join(", ", result) + "]";
+			}
+			case ITEM: {
+				ItemMeta meta = ((ItemStack) val.getVal()).getItemMeta();
+				
+				if(meta != null && meta.hasDisplayName())
+					return meta.getDisplayName();
+				else return "";
+			}
+			case POT: {
+				PotionEffect effect = (PotionEffect) val.getVal();
+				String name = StringUtils.capitalise(effect.getType().getName().replace('_', ' '));
+				
+				return name + " " + effect.getAmplifier() + " - " + formatTime("mm:s", effect.getDuration()/20d);
+			}
+			case LOC: {
+				Location loc = (Location) val.getVal();
+				if(loc.getYaw() == 0f && loc.getPitch() == 0f)
+					return "[" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + "]";
+				else return "[" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ", " + loc.getPitch() + ", " + loc.getYaw() + "]";
+			}
+			case SND: {
+				DFSound sound = (DFSound) val.getVal();
+				return sound.getName() + "[" + sound.pitch + "]" + "[" + sound.volume + "]";
+			}
+			default: {
+				return String.valueOf(val.getVal());
+			}
+		}
+	}
+	
+	public static String[] parseTxt(DFValue[] vals){
+		String[] result = new String[vals.length];
+		for(int i = 0; i < vals.length; i++) result[i] = parseTxt(vals[i]);
+		
+		return result;
+	}
+	
+	public static String formatTime(String format, double seconds){
+		long timestamp = (long) seconds * 1000;
+		SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+		return dateFormat.format(new Date(timestamp));
+	}
+	
+	public static String escapeRegex(String input){
+		return input.replaceAll("[-.+*?\\[^\\]$(){}=!<>|:\\\\]", "\\\\$0");
+	}
+	
+	public static HashMap<String, DFValue> getArgs(Object obj){
+		return (HashMap<String, DFValue>) obj;
+	}
+	
+	public static HashMap<String, String> getTags(Object obj){
+		return (HashMap<String, String>) obj;
+	}
+	
+	@EventHandler
+	public static void PlayerLeave(PlayerQuitEvent event){
+		if(Bukkit.getOnlinePlayers().size() == 1) DFVar.globalVars = new HashMap<>(); // Purge all global vars when all players leave
+	}
+	
+	@EventHandler
+	public static void ClickMenuSlot(InventoryClickEvent event){
+		if(inCustomInv((Player) event.getView().getPlayer())) event.setCancelled(true); // ClickSlot event triggered from inside custom GUI
+	}
+	
+	@EventHandler
+	public static void PlayerDmgPlayer(EntityDamageByEntityEvent event){
+		if(event.getDamager() instanceof Player && event.getEntity() instanceof Player){
+			if(!PlayerData.getPlayerData(((Player) event.getEntity()).getUniqueId()).canPvP)
+				event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public static void PlaceBlock(BlockPlaceEvent event){
+		PlayerData playerData = PlayerData.getPlayerData(event.getPlayer().getUniqueId());
+		if(!playerData.allowedBlocks.contains(event.getBlockPlaced().getType())) event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public static void BreakBlock(BlockBreakEvent event){
+		PlayerData playerData = PlayerData.getPlayerData(event.getPlayer().getUniqueId());
+		if(!playerData.allowedBlocks.contains(event.getBlock().getType())) event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public static void Death(PlayerDeathEvent event){
+		PlayerData playerData = PlayerData.getPlayerData(((Player) event.getEntity()).getUniqueId());
+		if(!playerData.deathDrops) event.getDrops().clear();
+		if(playerData.keepInv) event.setKeepInventory(true);
+		if(playerData.instantRespawn) ((Player) event.getEntity()).spigot().respawn();
+	}
+	
+	public static void getManagers(JavaPlugin plugin){
+		playerConfig = new FileManager(plugin, "playerData.yml");
+		varConfig = new FileManager(plugin, "varData.yml");
+	}
+	
 }
