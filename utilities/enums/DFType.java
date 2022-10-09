b5c18e920c4f11fe840670877be20710a178658c
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-10-09 18:45:13 -0500

    Particle Actions & Vector Data Structure

---
+++ utilities/enums/DFType.java
@@ -0,0 +1,15 @@
+package me.wonk2.utilities.enums;
+
+public enum DFType {
+	TXT,
+	NUM,
+	SND,
+	LOC,
+	ITEM,
+	POT,
+	VAR,
+	LIST,
+	VEC,
+	PART,
+	ANY // This is only used internally for parameter types.
+}
