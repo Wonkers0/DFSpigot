d08c6b863cb7c1a4e0008c348545d17dbdad8ad0
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-11-13 14:45:46 -0600

    Delete utilities directory

--- utilities/actions/pointerclasses/brackets/RepeatingBracket.java
+++
@@ -1,9 +0,0 @@
-package me.wonk2.utilities.actions.pointerclasses.brackets;
-
-import me.wonk2.utilities.actions.Repeat;
-
-public class RepeatingBracket extends Bracket{
-	public Repeat pointer;
-	
-	public RepeatingBracket(){}
-}
